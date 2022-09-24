a349b78e6670e409690f24bc153cfc67e0dcabdd
Serega1288 <svtomusjak@gmail.com>
2022-09-24 17:04:33 +0300

    done

--- gatsby/src/constants.js
+++ gatsby/src/constants.js
@@ -4,7 +4,7 @@ export  const  CATEGORY_LABEL_USE = 'USA';
 export  const  CATEGORY_LABEL_NETHERLANDS = 'Netherlands';
 
 export const  CATEGORY_LABELS_LIST = {
-    [CATEGORY_LABEL_ITALY] : 'Італія',
+    [CATEGORY_LABEL_ITALY] : 'Італія11',
     [CATEGORY_LABEL_USE] : 'США',
     [CATEGORY_LABEL_GREECE] : 'Греція',
     [CATEGORY_LABEL_NETHERLANDS] : 'Нідкрланди',
