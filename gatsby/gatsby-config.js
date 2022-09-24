a157b214fd0eb46308af0122162e120404bbd84d
Serega1288 <svtomusjak@gmail.com>
2022-09-24 19:50:41 +0300

    pwa config

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -34,6 +34,7 @@ module.exports = {
                 background_color: `#f7f0eb`,
                 theme_color: `#a2466c`,
                 display: `standalone`,
+                icon: `src/assets/img/gatsby-icon.png`,
             },
         },
         {
